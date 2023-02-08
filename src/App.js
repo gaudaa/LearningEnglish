d3e1971a576fc996cb289a2c4a94a6591e99ba3f
gzamka <g.zamka1@gmail.com>
2023-02-08 18:02:13 +0800

    merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import {Home} from './page/home/home.jsx'
+import { Login } from './page/Login'
 
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
         <Route path="/">
           <Route path="" element={<Navigate to="home"/>}></Route>
           <Route path="home" element={<Home/>}></Route>
+          <Route path="login" element={<Login/>}></Route>
+
         </Route>
       </Routes>
     </BrowserRouter>
