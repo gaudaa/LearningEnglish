45bbbcb85f1bdc373bfab844bf4cc2baa3e4d89d
gzamka <g.zamka1@gmail.com>
2023-02-08 19:34:33 +0800

    Login page

---
+++ src/page/Login.jsx
@@ -0,0 +1,26 @@
+
+export const Login = () =>{
+
+
+
+
+
+
+  
+    return (
+      <div style={{height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <div>
+
+            <input placeholder="Phonenumber" />
+            <button>code awah</button>
+        </div>
+        <div>
+
+            <input placeholder="Code oruulah" />
+            <button>Newtreh</button>
+        </div>
+      </div>
+    );
+  }
+  
+  
