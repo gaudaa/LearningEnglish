efce031864b041580cb299e0484b917aca3eddf1
gaudaa <g.ariunbold01@gmail.com>
2023-02-08 19:26:07 +0800

    header hiisen

--- src/router.js
+++ src/router.js
@@ -1,17 +1,15 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import {Home} from './page/home/home.jsx'
-import { Login } from './page/Login'
+import { Navigationcontainer } from "./page/navigationcontainer.jsx";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path="/" element={<Navigationcontainer/>}>
           <Route path="" element={<Navigate to="home"/>}></Route>
           <Route path="home" element={<Home/>}></Route>
-          <Route path="login" element={<Login/>}></Route>
-
         </Route>
       </Routes>
     </BrowserRouter>
