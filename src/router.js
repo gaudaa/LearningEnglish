5cb880dc7b904047b54ca7c1d241d1a4f01bf30b
gzamka <g.zamka1@gmail.com>
2023-02-08 18:02:13 +0800

    merge

--- src/router.js
+++ src/router.js
@@ -1,13 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import {Home} from './page/home/home.jsx'
-import { Navigationcontainer } from "./page/navigationcontainer.jsx";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigationcontainer/>}>
+        <Route path="/">
           <Route path="" element={<Navigate to="home"/>}></Route>
           <Route path="home" element={<Home/>}></Route>
         </Route>
