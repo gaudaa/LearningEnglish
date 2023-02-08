3232b2a55ace1cd1bb01246bb0b04e4fb78bac33
gzamka <g.zamka1@gmail.com>
2023-02-08 20:10:28 +0800

    Login connected

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,21 +1,50 @@
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { useEffect, useState, useRef } from 'react'
 
-export const Login = () =>{
+export const Login = () => {
 
-  
+    const [code, setCode] = useState('')
+    const [number, setNumber] = useState('')
+
+    const confirmationResult = useRef();
+    const recaptchaVerifier = useRef();
+
+    const auth = getAuth();
+    auth.languageCode = 'mn';
+
+useEffect(() => {
+       const auth = getAuth();
+       recaptchaVerifier.current = new RecaptchaVerifier('recaptcha-container', {}, auth);
+}, [])
+
+
+const codeAwah = async() => {
+    const phoneNumber = number;
+    const appVerifier = recaptchaVerifier.current;
+
+    const auth = getAuth();
+    confirmationResult.current = await signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+    console.log(number)
+    setNumber('')
+}
+
+
+const Submit = async() => {
+     console.log(await confirmationResult.current.confirm(code));
+
+}
+
     return (
-      <div style={{height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <div>
-
-            <input placeholder="Phonenumber" />
-            <button>code awah</button>
-        </div>
-        <div>
-
-            <input placeholder="Code oruulah" />
-            <button>Newtreh</button>
-        </div>
+      <div>
+        <input placeholder="phone number" onChange={(e)=> setNumber(e.target.value)} ></input>
+        <button onClick={codeAwah}> Code awah</button>
+        <input placeholder="code" onChange={(e)=> setCode(e.target.value)} ></input>
+        <button onClick={Submit} > Submit </button>
+        <div id="recaptcha-container"></div>
       </div>
     );
   }
+  
+  
   
   
