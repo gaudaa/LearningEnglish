aa02c2f892144d8224b66f01dbed488880b07d12
gaudaa <g.ariunbold01@gmail.com>
2023-02-09 23:23:11 +0800

    pg hiisen

---
+++ src/page/test/pg/pg6.jsx
@@ -0,0 +1,14 @@
+import "./pg.css"
+
+export const Pg6 = () => {
+    return(
+        <div className="main">
+            <div className="text">
+                <a><b>How would you like to learn?</b></a>
+                <button className="pt2">Self-study</button>
+                <button className="pt2">Live online classes</button>
+                <button className="pt2">A mix of both</button>
+            </div>
+        </div>
+    )
+}
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import {Home} from './page/home/home.jsx'
 import { Navigationcontainer } from "./page/navigationcontainer.jsx";
-import { Login } from './page/Login'
+import { Test } from "./page/test/test.jsx";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Route path="/" element={<Navigationcontainer/>}>
           <Route path="" element={<Navigate to="home"/>}></Route>
           <Route path="home" element={<Home/>}></Route>
-          <Route path="login" element={<Login/>}></Route>
+          <Route path="test" element={<Test/>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
